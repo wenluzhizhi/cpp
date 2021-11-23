645702620624d39cfae79b8c8895ea1a044e2071
wenlu <fang10159066099@163.com>
2021-11-23 23:39:48 +0800

    1123

---
+++ opengl_1/loadTexture/main.cpp
@@ -0,0 +1,21 @@
+#include<glad/glad.h>
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include "../include/shader_s.h"
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpengL", NULL, NULL);
+}
