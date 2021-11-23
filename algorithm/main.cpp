f58c4e6a40ac86129b1aa70adf0de87f6b4fe88c
wenlu <fang10159066099@163.com>
2021-11-24 00:02:09 +0800

    add_algorithm

---
+++ algorithm/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int testAr[] = {56, 3, 9, 21, 78, 234, 2, 91, 754,2};
+int size = 0;
+
+int main() {
+   size = sizeof(testAr) / sizeof(testAr[0]);
+   //cout << size << endl;
+
+   for(int i=0; i < size; i++) {
+       
+   }
+}
